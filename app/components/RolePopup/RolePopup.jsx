891b2240ccc28c7f8d6e6211d40e171ddfc4cb90
Nicholas Kann <nicholas.kann@gmail.com>
2025-04-23 15:45:47 -0400

    Added spy team feature
    Made it so spies can see the name of the other spies

--- app/components/RolePopup/RolePopup.jsx
+++ app/components/RolePopup/RolePopup.jsx
@@ -6,34 +6,57 @@ import Localized from '@components/Localized/Localized';
 import LocalizedLocation from '@components/LocalizedLocation/LocalizedLocation';
 import LocalizedRole from '@components/LocalizedRole/LocalizedRole';
 import { SPY_ROLE, SPY_LOCATION } from '@app/consts';
+import { useGameSpies } from '@app/selectors/gameSpies.js';
 
-export const RolePopup = ({ isOpen, toggle, player, location, role, customLocations }) => (
-  <Modal centered isOpen={isOpen} toggle={toggle}>
-    <ModalHeader tag="h3" toggle={toggle} className={`${styles.header} justify-content-center`} close={<button type="button" className="close" onClick={toggle}>&times;</button>}>
-      {player.name ?? player}
-    </ModalHeader>
-    <ModalBody className={styles.body}>
-      <Row className={styles.roleLine}>
-        <Col className="text-center">
-          <span className={styles.label}><Localized name="interface.location" />: </span>
-          <span className={styles.value}>
-            {role === SPY_ROLE ? SPY_LOCATION : <LocalizedLocation location={location} customLocations={customLocations} />}
-          </span>
-        </Col>
-      </Row>
-      {!!role && (
+export const RolePopup = ({ isOpen, toggle, player, location, role, customLocations }) => {
+  const spies = useGameSpies();
+  
+  // Filter out the current player from the spies list
+  const otherSpies = spies.filter(spy => {
+    const spyName = spy.name || spy;
+    const playerName = player.name || player;
+    return spyName !== playerName;
+  });
+  
+  return (
+    <Modal centered isOpen={isOpen} toggle={toggle}>
+      <ModalHeader tag="h3" toggle={toggle} className={`${styles.header} justify-content-center`} close={<button type="button" className="close" onClick={toggle}>&times;</button>}>
+        {player.name ?? player}
+      </ModalHeader>
+      <ModalBody className={styles.body}>
         <Row className={styles.roleLine}>
           <Col className="text-center">
-            <span className={styles.label}><Localized name="interface.role" />: </span>
+            <span className={styles.label}><Localized name="interface.location" />: </span>
             <span className={styles.value}>
-              {role === SPY_ROLE ? <SpyIcon /> : <LocalizedRole role={role} location={location} customLocations={customLocations} />}
+              {role === SPY_ROLE ? SPY_LOCATION : <LocalizedLocation location={location} customLocations={customLocations} />}
             </span>
           </Col>
         </Row>
-      )}
-    </ModalBody>
-  </Modal>
-);
+        {!!role && (
+          <Row className={styles.roleLine}>
+            <Col className="text-center">
+              <span className={styles.label}><Localized name="interface.role" />: </span>
+              <span className={styles.value}>
+                {role === SPY_ROLE ? <SpyIcon /> : <LocalizedRole role={role} location={location} customLocations={customLocations} />}
+              </span>
+            </Col>
+          </Row>
+        )}
+        
+        {role === SPY_ROLE && otherSpies && otherSpies.length > 0 && (
+          <Row className={styles.roleLine}>
+            <Col className="text-center">
+              <span className={styles.label}>Other Spy: </span>
+              <span className={styles.value}>
+                {otherSpies.map(spy => spy.name || spy).join(', ')}
+              </span>
+            </Col>
+          </Row>
+        )}
+      </ModalBody>
+    </Modal>
+  );
+};
 
 const styles = {
   roleLine: css({
--- app/selectors/gameSpies.js
+++ app/selectors/gameSpies.js
@@ -1,9 +1,20 @@
 import { useSelector } from 'react-redux';
 import { store } from '@app/store';
 
-export const gameSpiesSelector = (state) => state.game.spies;
+export const gameSpiesSelector = (state) => {
+  const spies = state.game.spies;
+  // console.log('Game spies from selector:', spies);
+  return spies;
+};
 
-export const getGameSpies = () => gameSpiesSelector(store.getState());
+export const getGameSpies = () => {
+  const spies = gameSpiesSelector(store.getState());
+  // console.log('Game spies from getGameSpies:', spies);
+  return spies;
+};
 
-export const useGameSpies = () =>
-  useSelector(gameSpiesSelector);
+export const useGameSpies = () => {
+  const spies = useSelector(gameSpiesSelector);
+  // console.log('Game spies from useGameSpies hook:', spies);
+  return spies;
+};
